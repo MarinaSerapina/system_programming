4a53afa7f2184b2bb7144bc08872180f4f386d58
MarinaSerapina <79499222+MarinaSerapina@users.noreply.github.com>
2021-10-24 19:13:11 +0300

    laba has done

---
+++ laba/cmake-build-release-visual-studio/CMakeFiles/lab1_fw.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_fw.dir/main.c.obj"
+  "CMakeFiles/lab1_fw.dir/splpv1.c.obj"
+  "lab1_fw.exe"
+  "lab1_fw.exe.manifest"
+  "lab1_fw.lib"
+  "lab1_fw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1_fw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
